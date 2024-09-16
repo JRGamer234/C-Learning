278c60dc87222fb945ff878487ba83c0708c22e8
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-09-16 13:09:49 +0200

    1

--- intro.cpp
+++ intro.cpp
@@ -1,5 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hola mundo");    
+    printf("Hola mundo");   
+	
+	int numero1;
+	int numero2 = 4;
+	
+	printf("Hola");
+	
+	numero1 = 2;
+	
+	printf("La suma de los numeros es %d", numero1 + numero2); 
 }
