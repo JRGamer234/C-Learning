b038f3d7a2dc7c2fc26e9d17df69c79e2b4ce5df
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-09-18 08:23:21 +0200

    1

--- intro.cpp
+++ intro.cpp
@@ -3,14 +3,14 @@
 int main(){
     printf("Hola mundo");   
 	
-	int numero1;
+	int numero1 = 2;
 	int numero2 = 4;
 	
 	printf("Hola");
 	
-	numero1 = 2;
+	int resultado = numero1 * numero2;
 	
-	printf("La suma de los numeros es %d", numero1 + numero2);
+	printf("La suma de los numeros es %d", resultado);
 	
 	// printf("La suma de %d y %d es %d", numero1, numero2, numero1 + numero2);  %d sustituye según el orden
 	
