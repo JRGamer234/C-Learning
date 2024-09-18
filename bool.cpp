dd61a4f4b04fd1cb179752159602626a483676f0
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-09-18 08:55:56 +0200

    1

--- bool.cpp
+++ bool.cpp
@@ -4,7 +4,7 @@ int main(){
 	
 	int numero1;
 	int numero2;
-	
+	/*
 	// bool verdadero = true
 	bool verdadero = 1;
 	
@@ -32,7 +32,7 @@ int main(){
 	}else{
 		printf("En la variable numero hay un 4");
 	} */
-	
+	/*
 	printf("Introduce un numero: ");
 	scanf("%d", &numero1);
 	
@@ -40,7 +40,33 @@ int main(){
 	scanf("%d", &numero2);
 	
 	printf("La sume de los numeros es: %d", numero1 + numero2);
-	
+	*/
 	
 
+  	/*
+		Programa que pida 3 numeros y los sume, al hacer la suma tiene que mostrar un mensaje diciendo si es (mayor o igual) a 100 el resultado de la suma 
+	*/
+	
+	int num1;
+	int num2;
+	int num3;
+	int resultado;
+	
+		printf("Introduce un numero: ");
+	scanf("%d", &num1);
+		printf("Introduce otro numero: ");
+	scanf("%d", &num2);
+		printf("Introduce el último numero: ");
+	scanf("%d", &num3);
+	
+	resultado = num1 + num2 + num3;
+	
+	if(resultado ==100){
+		printf("El resultado es igual a 100");
+	}else if(resultado > 100){
+		printf("El resultado es mayor a 100");
+	}else{
+		printf("El resultado es menor a 100");
+	}
+	
 }
