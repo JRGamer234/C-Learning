3a3cbc12285cc82440df85f4248328b8e7eea9e6
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-09-24 10:47:23 +0200

    1

--- Prueba.cpp
+++ Prueba.cpp
@@ -1,30 +1,34 @@
-int num1, num2, num3;
-printf("Numero 1: ");
-scanf("%d", &num1);
-printf("Numero 2: ");
-scanf("%d", &num2);
-printf("Numero 3: ");
-scanf("%d", &num3);
+#include<stdio.h>
 
-if(num1 < num2){
-	if(num2 < num3){
-		printf(num1, "<", num2, "<", num3);
-	}else{
-		print(num1, "<", num3, "<", num2);
-	}
-}else if(num2 < num1){
-	if(num1 < num3){
-		printf(num2, "<", num1, "<", num3);
-	}else{
-		printf(num2, "<", num3, "<", num1);
-	}
-}else if(num3 < num1){
-	if(num1 < num2){
-		printf(num3, "<", num1, "<", num2);
-	}else{
-		printf(num3, "<", num2, "<", num1);
-	}
-}else{
-	printf("ERROR");
+int main() {
+    int num1, num2, num3;
+    printf("Numero 1: ");
+    scanf("%d", &num1);
+    printf("Numero 2: ");
+    scanf("%d", &num2);
+    printf("Numero 3: ");
+    scanf("%d", &num3);
+
+    if (num1 <= num2 && num1 <= num3) {
+        if (num2 <= num3) {
+            printf("%d < %d < %d\n", num1, num2, num3);
+        } else {
+            printf("%d < %d < %d\n", num1, num3, num2);
+        }
+    } else if (num2 <= num1 && num2 <= num3) {
+        if (num1 <= num3) {
+            printf("%d < %d < %d\n", num2, num1, num3);
+        } else {
+            printf("%d < %d < %d\n", num2, num3, num1);
+        }
+    } else {
+        if (num1 <= num2) {
+            printf("%d < %d < %d\n", num3, num1, num2);
+        } else {
+            printf("%d < %d < %d\n", num3, num2, num1);
+        }
+    }
+
+    return 0;
 }
 
