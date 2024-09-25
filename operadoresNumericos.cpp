64d1e64a3b704bdba04699f9fc65ed5862371c32
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-09-25 08:37:49 +0200

    5

---
+++ operadoresNumericos.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(){
+
+char letra = 'a';
+char letra1 = '3';
+
+printf("%f \n", letra);
+
+
+int numero1 = 3;
+
+// if(numero == 3) son iguales if(numero1 == 51)
+//if('3' == 51)
+ 
+
+// Diferente
+if(numero1 == 3){
+	printf("En la variable hay un 3");
+}else if(numero1 == 2){
+	printf("En la variable hay un 2");
+}else{
+	printf("En la variable no hay ni un 3 ni un 2");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
