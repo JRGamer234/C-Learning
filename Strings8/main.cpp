c99d669bddc3ca54c4701c0322bde1d8cae19cfd
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-10-14 13:06:39 +0200

    1

---
+++ Strings8/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	/*
+	Pedir por teclado numeros hasta que:
+		El usuario introduzca 0 o El usuario introduzca 20 numeros
+	Luego imprimimos por pantalla todos los numeros pares introducidos
+	*/
+	
+	
+	
+		
+	
+	return 0;
+}
