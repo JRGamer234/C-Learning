7356431fae194fa37545f696e2b249664648d0ca
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-10-16 08:50:29 +0200

    1

---
+++ Randoms9/main.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	/*
+	Generar 20 numeros impares de 1 a 100 y mostrarlos por
+	pantalla ademas indicar cual es el numero mas grande y el mas pequeño
+	*/
+int num[20];
+int max, min;
+srand(time(NULL));
+
+for(int i = 0; i < 20; i ++){
+num[i] = (rand() % 99) + 1;
+printf("%d\n", num[i]);
+}
+
+max = min = num[0];
+
+for(int i = 0; i < 20; i ++){
+if(num[i] > max){
+max = num[i];
+}else if(num[i] < min){
+min = num[i];
+}
+}
+printf("Max = %d \n", max);
+printf("Min = %d\n", min);
+}
