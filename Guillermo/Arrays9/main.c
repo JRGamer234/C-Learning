ad7c86b711e5ac4feec5a4f134e767f273555aa0
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-10-18 08:25:13 +0200

    1

---
+++ Guillermo/Arrays9/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	/*
+	-Pide un número N y pinta los números desde N hasta 1000
+	-Dado un número N por teclado imprimir todos los divisores que tiene. Por ejemplo, si N=12, debe imprimir 1,2,3,4,6 y 12
+	-Dada una cadena imprimir su longitud sin usar la función LONGITUD.
+	-Dadas dos cadenas C y D indicar si son bifrontes (Por ejemplo, C=amor y D=roma son bifrontes)
+	-Dada una cadena mostrar cuántas veces aparece en ella cada vocal
+	-Dada una cadena mostrar cuántas veces aparece en ella cada consonante
+	-Dada una cadena determinar si es un PANGRAMA (usa todas las vocales y consonantes aunque haya repeticiones)
+	-Dada una cadena determinar si es un PANGRAMA  PERFECTO (usa todas las vocales y consonantes SIN repeticiones)
+	-Dada una cadena C y un número N mostrar la cadena resultante de aplicar a C una codificación basada en cifrado César dedesplazamiento N
+	*/
+	
+	
+	
+	return 0;
+}
--- Guillermo/Robot8/main.c
+++ Guillermo/Robot8/main.c
@@ -1,38 +1,110 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int print_map(int robot_i, int robot_j, int gas_i, int gas_j, int map_size);
+int move_robot(char direction, int *robot_i, int *robot_j, int *gasoline, int map_size, int gas_cost);
 
-int main(int argc, char *argv[]) {
-	/*
-	En un mundo circular y bidimensional de 10 x 10, un robot comienza en una posición dada por el usuario.
-	-El robot, puede moverse en las direcciones: norte, sur, este y oeste.
-	-El robot comienza con 100 unidades de gasolina.
-	-Cada vez que se mueve una casilla gasta 10 unidades de gasolina.
-	-Aleatoriamente, aparece en otra posición del mapa, un bidón de gasolina. Cada vez que el robot tome el bidón, recarga a 100 su depósito de gasolina.
-	-Se debe imprimir un mapa en caracteres ASCII indicando la posición del robot, así como una interfaz rudimentaria que muestre la posición, la gasolina restante y las opciones de movimiento al usuario.
-	*/
-	
-	int posi= 0, posj = 0;
+int main() {
+    int robot_i, robot_j;
+    int gas_i, gas_j;
+    int gasoline;
 
-	printf("Dime una posicion fila: ");
-	scanf("%d", &posi);
-	printf("Dime una posicion columna: ");
-	scanf("%d", &posj);
-	
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-		
-			if(i == posi && j == posj){
-				printf(" R");
-			}else{
-				printf(" -");
-			}
-		}
-		printf("\n");
-	}
-	
-	
-	return 0;
+    const int map_size = 10;
+    const int max_gas = 100;
+    const int gas_cost = 10;
+
+    printf("Dime una posicion fila (0-%d): ", map_size - 1);
+    scanf("%d", &robot_i);
+    printf("Dime una posicion columna (0-%d): ", map_size - 1);
+    scanf("%d", &robot_j);
+
+    gasoline = max_gas;
+    
+	gas_i = rand() % map_size;
+    gas_j = rand() % map_size;
+    
+	while (gas_i == robot_i && gas_j == robot_j) {
+        gas_i = rand() % map_size;
+        gas_j = rand() % map_size;
+    }
+
+    while (gasoline > 0) {
+        if (print_map(robot_i, robot_j, gas_i, gas_j, map_size) != 0) {
+            printf("Error al imprimir el mapa.\n");
+            return 1;
+        }
+        
+        printf("Posicion actual: (%d, %d)\n", robot_i, robot_j);
+        printf("Gasolina restante: %d\n", gasoline);
+        printf("Moverse (n= norte, s= sur, e= este, o= oeste, q= salir): ");
+        char move;
+        scanf(" %c", &move);
+
+        if (move == 'q') {
+            printf("Saliendo del juego.\n");
+            break;
+        }
+
+        if (move_robot(move, &robot_i, &robot_j, &gasoline, map_size, gas_cost) != 0) {
+            printf("Movimiento no valido.\n");
+            continue;
+        }
+
+        if (robot_i == gas_i && robot_j == gas_j) {
+            printf("¡Has encontrado un bidón de gasolina! Gasolina recargada a %d.\n", max_gas);
+            gasoline = max_gas;
+            
+			gas_i = rand() % map_size;
+            gas_j = rand() % map_size;
+            while (gas_i == robot_i && gas_j == robot_j) {
+                gas_i = rand() % map_size;
+                gas_j = rand() % map_size;
+            }
+        }
+        if (gasoline <= 0) {
+            printf("¡Te has quedado sin gasolina! Fin del juego.\n");
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int print_map(int robot_i, int robot_j, int gas_i, int gas_j, int map_size) {
+    for (int i = 0; i < map_size; i++) {
+        for (int j = 0; j < map_size; j++) {
+            if (i == robot_i && j == robot_j) {
+                printf(" R"); 
+            } else if (i == gas_i && j == gas_j) {
+                printf(" G"); 
+            } else {
+                printf(" -"); 
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int move_robot(char direction, int *robot_i, int *robot_j, int *gasoline, int map_size, int gas_cost) {
+    switch (direction) {
+        case 'n':
+            *robot_i = (*robot_i - 1 + map_size) % map_size;
+            break;
+        case 's':
+            *robot_i = (*robot_i + 1) % map_size;
+            break;
+        case 'e':
+            *robot_j = (*robot_j + 1) % map_size;
+            break;
+        case 'o':
+            *robot_j = (*robot_j - 1 + map_size) % map_size;
+            break;
+        default:
+            return 1;
+    }
+    *gasoline -= gas_cost;
+    return 0;
 }
