0dc64f5ec48cac5f649c6b00c25a7200e01ad437
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-10-23 13:35:37 +0200

    1

--- Ahorcado(Strings)11/main.cpp
+++ Ahorcado(Strings)11/main.cpp
@@ -25,5 +25,24 @@ int main(int argc, char** argv) {
 	for(i = 0; i < strlen(palabra); i++){
 		printf("%c", palabra[i]);
 	}
+	
+	
+	/*programa que pida una palabra por teclado y convierta cada letra de mayuscula a minuscula o minuscula a mayuscula (letra por letra)
+
+	ejemplo:
+
+	hoLA
+
+	HOLA
+	*/
+
+	/*Programa que pida una palabra por teclado y te diga cuantas vocales tiene la palabra en total*/
+
+	/*Programa que pida una palabra por teclado y te diga cuantas veces aparece CADA vocal*/
+
+	/*Programa que pida 2 palabras por teclado y solo muestre las letras que aparecen en las 2 palabras*/
+	
+	
+	
 	return 0;
 }
