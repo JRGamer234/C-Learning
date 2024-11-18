79ee151249865bfb0013dec6aea55a86e4bb056a
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-11-18 09:20:35 +0100

    1

---
+++ Guillermo/PropuestaEstudio2/ProEs.c
@@ -0,0 +1,15 @@
+// Programa que solicite dos arrays de 10 posiciones numericas
+// y sea capaz de almacenar en un tercer array la suma de sus celdas
+// EJ:            9573162310
+//                 0413734235
+//                 9986896545
+
+// Programa que sea capaz de detectar la secuencia 1 2 3
+// en un array numerico de 10 posiciones
+// Ej: 9595123234 --->Por pantalla se devuelve: SI, ESTA LA SECUENCIA
+// Ej: 9595023234 --->Por pantalla se devuelve: NO, ESTA LA SECUENCIA
+
+// Hacer un programa parecido al anterior, donde se detecte
+// la secuencia 123 pero esta vez en un numero entero
+// Ej: 91235 --->Por pantalla se devuelve: SI, ESTA LA SECUENCIA
+// Ej: 21379 --->Por pantalla se devuelve: NO, ESTA LA SECUENCIA
