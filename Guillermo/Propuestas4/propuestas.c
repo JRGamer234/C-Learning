ebf99ed1f3c9cfccbc9edfd4ff78a55036899247
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-11-26 10:33:45 +0100

    1

---
+++ Guillermo/Propuestas4/propuestas.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+
+
+/*-Programa que reciba dos palabras y saque una tercera alternando sus letras. Si las palabras tienen diferente longitud, la cadena más larga se imprimirá hasta el final tras agotar las letras de la palabra corta.
+Ej: "poblado" y "ciudadela" traen a la salida: "pcoibuldaaddoela"
+*/
+
+
+
+
+/*
+-Programa que pida introducir dos valores de la clásica ecuación Espacio = Tiempo x Velocidad. Dándole a elegir al usuario que dos variables va a leer, y calcular la tercera.
+Solicitar la repetición del programa, teniendo en cuenta que los valores calculados anteriormente sean almacenados de alguna forma y se vaya actualizando una media de las tres variables por separado. Mostrar dichas medias cada iteración.
+*/
+
+
+/*
+-Divisor de cadenas:
+Programa que pida una palabra y un numero. A la salida se debe dividir la palabra en dos cadenas diferentes por la posición indicada en el número
+Ej: "ecosistema" y el número 4
+A la salida imprimo dos cadenas "ecos" y "istema"
+*/
+
+
+
+
+}
