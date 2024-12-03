4c0951b91cf93dc7b0a69b0219d78514c2211f3a
JRGamer234 <ramosgomezjorge1@gmail.com>
2024-12-03 10:24:57 +0100

    1

---
+++ Ejer-Preexamen/preexamen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+Crea un programa en C que implemente una codificación basada en invertir las palabras de una frase. El programa debe:
+
+Definir una variable palabra[50] =
+"Hola mundo desde C";
+Dividir la frase en palabras (considera los espacios como separadores).Invertir cada palabra individualmente.Mostrar la frase codificada con las palabras invertidas, pero en el mismo orden.
+
+    palabra[50]: "Hola mundo desde C"
+    Salida: "aloH odnum edsed C"
+    Usa funciones para modularizar el código:
+    Una función para invertir una palabra.
+    Una función para dividir la frase en palabras y procesarlas.
+*/
