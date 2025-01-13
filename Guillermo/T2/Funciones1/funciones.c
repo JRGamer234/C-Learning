032fc7703ab628ceabf28872b545848ca8ed9e5d
JRGamer234 <ramosgomezjorge1@gmail.com>
2025-01-13 10:08:05 +0100

    1

---
+++ Guillermo/T2/Funciones1/funciones.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+/*-Procedimiento que pinte un mensaje por pantalla enmarcado en asteriscos*/
+
+
+/*-Función que devuelva la media de dos números.*/
+
+
+/*-Función que devuelva la media de un array de números (quizás necesites un parámetro que diga cuantos números de ese array intervengan en esa media)*/
+
+
+/*-Función que devuelva el último caracter de una palabra.*/
+
+
+/*-Procedimiento que Imprima cualquier tablero de caracteres de un array doble (puedes especificar con parámetros de entrada aparte las filas/columnas del array).*/
+
+
+/*-Función que devuelva verdadero o falso si un número es divisible por otro.*/
+
+
+/*-Función que devuelva verdadero o falso si un número es primo.*/
+
+
+/*-Función que devuelva el menor número de un array.*/
+
+
+/*-Encapsula en una función el algoritmo del Palíndromo.*/
+
+
+/*-Procedimiento que reciba una palabra y se sustituyan sus vocales por otra letra pasada por parámetro.*/
+
+
+/*-Función/Procedimiento que ordene un array de números de menor a mayor*/
+
