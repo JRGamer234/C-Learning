83d65d265d954f747629d1f059c18adf8aff4f93
JRGamer234 <ramosgomezjorge1@gmail.com>
2025-01-14 11:47:28 +0100

    1

--- Guillermo/T2/Funciones1/funciones.c
+++ Guillermo/T2/Funciones1/funciones.c
@@ -2,34 +2,101 @@
 #include <string.h>
 
 /*-Procedimiento que pinte un mensaje por pantalla enmarcado en asteriscos*/
+int pintarMensaje()
+{
+    char nombre[50];
+    printf("Introduce tu nombre: ");
+    scanf("%s", nombre);
 
+    // Arriba
+    for (int i = 0; i < strlen(nombre) + 2; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 
+    // Nombre
+    printf("#%s#\n", nombre);
+
+    // Abajo
+    for (int i = 0; i < strlen(nombre) + 2; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
 /*-Función que devuelva la media de dos números.*/
-
+void Media(float n1, float n2, float *r)
+{
+    *r = (n1 + n2) / 2;
+}
 
 /*-Función que devuelva la media de un array de números (quizás necesites un parámetro que diga cuantos números de ese array intervengan en esa media)*/
+float calcularMedia(int array[], int cantidad){
+    if (cantidad <= 0)
+    {
+        return 0; // Evitar división por cero
+    }
+    int suma = 0;
 
+    // Sumar
+    for (int i = 0; i < cantidad; i++)
+    {
+        suma += array[i];
+    }
 
+    return (float)suma / cantidad;
+}
 /*-Función que devuelva el último caracter de una palabra.*/
 
-
+char UltimoCaracter(char palabra[]){
+    return palabra[strlen(palabra) - 1];
+}
 /*-Procedimiento que Imprima cualquier tablero de caracteres de un array doble (puedes especificar con parámetros de entrada aparte las filas/columnas del array).*/
-
 
 /*-Función que devuelva verdadero o falso si un número es divisible por otro.*/
 
-
 /*-Función que devuelva verdadero o falso si un número es primo.*/
-
 
 /*-Función que devuelva el menor número de un array.*/
 
-
 /*-Encapsula en una función el algoritmo del Palíndromo.*/
-
 
 /*-Procedimiento que reciba una palabra y se sustituyan sus vocales por otra letra pasada por parámetro.*/
 
-
 /*-Función/Procedimiento que ordene un array de números de menor a mayor*/
 
+void main(){
+    // 1
+    pintarMensaje();
+
+    // 2
+    float a, b, resultado;
+
+    a = 7;
+    b = 6;
+
+    Media(a, b, &resultado);
+    printf("%f", resultado);
+    printf("\n");
+    // 3
+    int numeros[] = {10, 20, 30, 40, 50};
+    int longitud = 5;
+
+    // Calcular media
+    float mediaTotal = calcularMedia(numeros, longitud);
+    printf("Media de todos los números: %.2f\n", mediaTotal);
+
+    printf("\n");
+    // 4
+    char palabra[100];
+    
+    printf("Introduce una palabra: ");
+    scanf("%s", palabra);
+    
+    char ultimo = UltimoCaracter(palabra);
+    
+    printf("El ultimo caracter de %s es: %c\n", palabra, ultimo);
+
+
+}
