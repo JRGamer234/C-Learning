3a849c0eed74449fbf89b4473890333597a9a76c
JRGamer234 <75247543+JRGamer234@users.noreply.github.com>
2025-03-26 09:13:40 +0100

    1

--- Andrés/Libreria/libreria.h
+++ Andrés/Libreria/libreria.h
@@ -1,7 +1,7 @@
 
 int strlen(char *palabra){
     int i = 0;
-    for(i=0; palabra[i] != '\0'; i++){
+    for(i=0; palabra != '\0'; i++){
         palabra++;
     };
     return i;
---
+++ prueba.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <libreria.h>
+
+int main() {
+    char palabra[50] = "hola";
+
+    printf("La longitud de la palabra es: %d\n", strlen(palabra));
+}
