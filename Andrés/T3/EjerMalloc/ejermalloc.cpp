906589ff81f76e22874a85e984446297439e4c24
JRGamer234 <75247543+JRGamer234@users.noreply.github.com>
2025-04-30 09:59:17 +0200

    1

--- Andrés/T3/EjerMalloc/ejermalloc.cpp
+++ Andrés/T3/EjerMalloc/ejermalloc.cpp
@@ -1,24 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(){
-    /*Guardar 5 nombres y notas de 5 alumnos usando malloc */
+typedef struct {
+    char nombres[5];
+    int notas[5];
+} Alumnos;
 
-    char *nombres;
-    int *notas;
-
-    nombres = (char **)malloc(NUM_ALUMNOS * sizeof(char *));
-
-    for(int i = 0; i < 5; i++){
-        printf("Introduce el nombre del alumno ", (i+1), ": ");
-        scanf("%s", nombres[i]);
-        printf("Introduce la nota: ");
-        scanf("%s", notas[i]);
-    }
-
-    for(int i = 0; i < 5; i++){
-        printf("Nombre del alumno: %s", nombres[i], " -->  Nota: %d", notas[i]);
-    }
+int main() {
+    Alumnos grupo;
     
+    char aux[100];
+    int auxNum;
+
+    grupo.nombres = (char **)malloc(5 * sizeof(char *));
+    grupo.notas = (int *)malloc(5 * sizeof(int));
+
+    for (int i = 0; i < 5; i++) {
+        printf("Introduce el nombre del alumno %d: ", i + 1);
+        scanf("%s", aux);
+
+        printf("Introduce la nota: ");
+        scanf("%d", &auxNum);
+        
+        grupo.nombres[i] = (char *) malloc(sizeof(char)*strlen(aux));
+        grupo.notas[i] = (int *) malloc(sizeof(int));
+        
+        *grupo.notas[i] = auxNum;
+        strcpy(grupo.nombres[i], aux);
+    }
+    printf("\nResultados de los alumnos:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Nombre del alumno: %s --> Nota: %d\n", grupo.nombres[i], grupo.notas[i]);
+    }
+    for (int i = 0; i < 5; i++) {
+        free(grupo.nombres[i]);
+    }
+    free(grupo.nombres);
+    free(grupo.notas);
+
+    return 0;
 }
+
