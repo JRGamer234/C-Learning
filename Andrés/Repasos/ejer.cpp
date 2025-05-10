c87f9c335994a8bdb3e207624111a5e211290fb2
JRGamer234 <75247543+JRGamer234@users.noreply.github.com>
2025-05-10 14:34:00 +0200

    1

---
+++ Andrés/Repasos/ejer.cpp
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void ejer1(){
+    int num[10];
+    int sum = 0;
+    for(int i =0 ; i < 10 ; i++){
+        printf("Introduce un numero (%d): ", (i+1));
+        scanf("%d", &num[i]);
+        sum += num[i];
+    }
+    printf("La suma de los numeros es: %d\n", sum);
+}
+
+void ejer2(){
+    int num2[20];
+    int mayor=0,menor=0;
+    for(int i =0 ; i < 20 ; i++){
+        printf("Introduce un numero (%d): ", (i+1));
+        scanf("%d", &num2[i]);
+    }
+    for(int i =1; i<20;i++){
+        if(num2[i]>mayor){
+            mayor=num2[i];
+        }
+        if(num2[i]<menor){
+            menor=num2[i];
+        }
+    }
+    printf("El mayor es: %d\n", mayor);
+    printf("El menor es: %d\n", menor);
+}
+
+void ejer3(){
+    int nota[30];
+    int promedio = 0;
+    int suma = 0;
+    int est = 0;
+    for(int i = 0; i < 30 ; i++){
+        printf("Introduce una nota (%d): ", (i+1));
+        scanf("%d", &nota[i]);
+        suma += nota[i];
+    }
+    promedio = suma / 30;
+    for (int i = 0; i < 30; i++){
+        if(nota[i] > promedio){
+            est++;
+        }
+    }
+    printf("El promedio de las notas es: %d\n", promedio);
+    printf("El numero de estudiantes que superaron el promedio es: %d\n", est);
+
+}
+
+void ejer4(){
+    
+}
+
+
+int main(){
+    ejer3();
+
+}
