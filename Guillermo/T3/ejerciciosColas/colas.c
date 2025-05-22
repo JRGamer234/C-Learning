ab17e45d4e8a83549edd80ea8b61c9a5b1e1742e
JRGamer234 <75247543+JRGamer234@users.noreply.github.com>
2025-05-22 12:00:18 +0200

    1

---
+++ Guillermo/T3/ejerciciosColas/colas.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int verdadero(){
+    
+}
+
+int main(){
+/*-Función que devuelva Verdadero o Falso si una lista enlazada contiene un dato int pasado por párametro de entrada
+-Función que devuelva la suma de los datos enteros de una lista enlazada.
+-Función que elimine el nodo de orden especificado por parámetro.
+(Ejemplo: EliminarNodoN(miLE,2); suprime el segundo nodo de la lista)
+-Función que inserte un nodo en el orden especificado por parámetro.
+(Ejemplo: InsertarNodoN(miLE,2,7); inserta detrás del segundo nodo de la lista un nuevo nodo con un nuevo dato 7)
+-Función que elimine la mitad final de una lista enlazada.
+(En caso de haber un numero de impar de nodos, se deja al programador decidir si eliminar o no el nodo central)
+-Función que elimine la mitad inicial de una lista enlazada.
+(En caso de haber un numero de impar de nodos, se deja al programador decidir si eliminar o no el nodo central)
+-Función que intercambie de posición el nodo inicial y el nodo final de una lista enlazada.
+-Función que intercambie de posición dos nodos cuya posición venga dada por parámetros
+(Ejemplo: IntercambiarNodos(miLE,3,5); intercambia de posición los nodos tercero y quinto)
+-Función que concatene dos listas enlazadas.
+-Función que elimine los nodos de una lista enlazada que contengan un número par.
+-Función que invierta el orden de los nodos de una lista enlazada.
+
+-Función que invierta el orden de elementos de una pila.
+-Función que extraiga el elemento del fondo de una pila.
+-Función que mezcle intercaladamente el contenido de dos pilas.
+-Función que intercambie el elemento del fondo de una pila con su cima.
+-Función que solo permita introducir un elemento en la pila si ese elemento es la suma de los que ya están dentro.
+
+-Función que adelante al último al primer lugar(cima) en una cola
+-Función que retrase al primero al último lugar en una cola
+-Función que, dada una cola, extraiga la mitad de atrás de dicha cola y la introduzca en una nueva.
+-Dadas dos colas, integrar en una nueva sus elementos de forma intercalada.*/
+}
